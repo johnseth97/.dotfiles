f637b43134869fefb40aebdcfd031d7a254a4c64
johnseth97 <17620345+johnseth97@users.noreply.github.com>
2024-09-12 20:41:32 -0400

    ZSH-plugin installation script

---
+++ .bin/install-plugins.sh
@@ -0,0 +1,11 @@
+#! /bin/zsh
+
+# Autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# You-Should-Use
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
+
