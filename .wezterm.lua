be864bc33ae5312ccc899ae54f19649f1a6a99fb
johnseth97 <17620345+johnseth97@users.noreply.github.com>
2024-10-17 18:35:01 -0400

    Final wezterm config edits
    Because I've got ghostty now

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,23 +1,34 @@
 -- Pull in the wezterm API
+
+-- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
-local config = wezterm.config_builder()
+return {
+	-- Fonts and rendering
+	--
+	-- Sometimes needed on macos if using nix
+	-- front_end = "WebGpu",
+	--
+	-- Font and size, obviously
+	--
+	font = wezterm.font_with_fallback({
+		"Lilex Nerd Font Mono",
+		weight = "Regular",
+		stretch = "Normal",
+	}),
+	font_size = 13,
 
--- This is where you actually apply your config choices
+	-- Window setup
+	--
+	--Remove top bar
+	window_decorations = "RESIZE",
+	--Remove tab bar unless tabs are present
+	hide_tab_bar_if_only_one_tab = true,
 
--- Fonts and rendering
-config.front_end = "WebGpu"
-config.font = LilexNerdFontMono
-config.font_size = 12.5
-
--- Window setup
-config.window_decorations = "RESIZE"
-config.hide_tab_bar_if_only_one_tab = true
-
--- Color schemes
-config.color_scheme = "BlulocoDark"
-config.window_background_opacity = 1.0
-
--- and finally, return the configuration to wezterm
-return config
+	-- Color schemes and transparency
+	--
+	color_scheme = "BlulocoDark",
+	--
+	--Optional transparency
+	window_background_opacity = 1.0,
+}
