13cccb1f42759811dbd6ba0f90020d723b1800e6
johnseth97 <17620345+johnseth97@users.noreply.github.com>
2025-03-22 02:06:27 -0400

    fixed linux 1pw sign path

--- .config/zsh/linux.zsh
+++ .config/zsh/linux.zsh
@@ -8,11 +8,13 @@ if [ -S "$HOME/.1password/agent.sock" ]; then
   export SSH_AUTH_SOCK="$HOME/.1password/agent.sock"
 fi
 
+
+
 # 1Password Git setup (if available)
-if command -v op &>/dev/null && [ -f "/usr/local/bin/op-ssh-sign" ]; then
+if command -v op &>/dev/null && [ -f "/opt/1Password/op-ssh-sign" ]; then
   export GIT_SSH_COMMAND="ssh"
   git config --global gpg.format ssh
-  git config --global gpg.ssh.program "/usr/local/bin/op-ssh-sign"
+  git config --global gpg.ssh.program "/opt/1Password/op-ssh-sign"
 else
   echo "[1Password] Skipping Git integration - 1Password CLI or op-ssh-sign missing."
 fi
